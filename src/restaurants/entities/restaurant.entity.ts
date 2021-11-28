9272cf430d236737a1707330413dbf6bdbe397ee
Kyeongweon Park <kyeongweonpark@Kyeongweonui-MacBookAir.local>
2021-11-29 06:22:32 +0900

    middleware

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,31 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  @Length(5)
+  name: string;
+
+  @Field((type) => Boolean, { nullable: true })
+  @Column({ default: true })
+  @IsOptional()
+  @IsBoolean()
+  isVegan: boolean;
+
+  @Field((type) => String, { defaultValue: 'Seoul' })
+  @Column()
+  address: string;
+
+  @Field((type) => String)
+  @Column()
+  ownerName: string;
+}
